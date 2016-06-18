8b24ace3740cecc32f4b80ba4a45b6a14b9017b2
ay65535 <ay65535@icloud.com>
2016-06-19 01:07:48 +0900

    Add common alias

--- .zsh/30_aliases.zsh
+++ .zsh/30_aliases.zsh
@@ -37,6 +37,8 @@ alias lu='ls -ltur'        # Sort by and show access time, most recent last
 alias lt='ls -ltr'         # Sort by date, most recent last
 alias lr='ls -lR'          # Recursive ls
 
+alias q='exit'
+
 # The ubiquitous 'll': directories first, with alphanumeric sorting:
 #alias ll='ls -lv --group-directories-first'
 
