63775f1bddcf610f2693d8601b05a9abc95f7091
ay65535 <ay65535@icloud.com>
2016-07-17 09:49:07 +0900

    Add deploy cmd for win;Oops (+1 squashed commit) Squashed commits: [e271479] Add deploy cmd for win

---
+++ bin/make-deploy.bat
@@ -0,0 +1,26 @@
+cd %UserProfile%
+
+mklink .inputrc %UserProfile%\.dotfiles\.inputrc
+mklink .ackrc %UserProfile%\.dotfiles\.ackrc
+mklink .path %UserProfile%\.dotfiles\.path
+mklink .gitconfig %UserProfile%\.dotfiles\.gitconfig
+mklink /d .modules %UserProfile%\.dotfiles\.modules
+mklink .bashrc %UserProfile%\.dotfiles\.bashrc
+mklink .ptconfig.toml %UserProfile%\.dotfiles\.ptconfig.toml
+mklink .screenrc %UserProfile%\.dotfiles\.screenrc
+mklink .vimrc %UserProfile%\.dotfiles\.vimrc
+mklink /d .zsh %UserProfile%\.dotfiles\.zsh
+mklink .dir_colors %UserProfile%\.dotfiles\.dir_colors
+mklink /d .peco %UserProfile%\.dotfiles\.peco
+mklink .zshenv %UserProfile%\.dotfiles\.zshenv
+mklink .curlrc %UserProfile%\.dotfiles\.curlrc
+mklink .bash_profile %UserProfile%\.dotfiles\.bash_profile
+mklink .tmux.conf %UserProfile%\.dotfiles\.tmux.conf
+mklink /d .config %UserProfile%\.dotfiles\.config
+mklink /d .tmuxinator %UserProfile%\.dotfiles\.tmuxinator
+mklink .zshrc %UserProfile%\.dotfiles\.zshrc
+mklink .gitattributes %UserProfile%\.dotfiles\.gitattributes
+mklink /d .vim %UserProfile%\.dotfiles\.vim
+mklink .tigrc %UserProfile%\.dotfiles\.tigrc
+mklink .gitignore %UserProfile%\.dotfiles\.gitignore
+mklink /d bin %UserProfile%\.dotfiles\bin
